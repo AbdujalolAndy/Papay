e3f3c9a5c07c42896d29830c7feb924757333424
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-10-14 03:58:40 +0900

    feat: Modify member schema

--- schema/member.control.js
+++ schema/member.control.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
-const { member_type_enums, member_status_enums } = require("../lib/config");
+const {
+  member_type_enums,
+  member_status_enums,
+  ordenary_enums,
+} = require("../lib/config");
 
 const memberSchema = new mongoose.Schema({
   mb_nick: {
@@ -37,11 +41,11 @@ const memberSchema = new mongoose.Schema({
       message: "{VALUE} is not among permitted values",
     },
   },
-  mb_full_name: {
-    type: String,
-    required: true,
-  },
   mb_address: {
+    type: String,
+    required: false,
+  },
+  mb_description: {
     type: String,
     required: false,
   },
@@ -54,6 +58,36 @@ const memberSchema = new mongoose.Schema({
     required: false,
     default: 0,
   },
+  mb_top: {
+    type: String,
+    required: false,
+    default: "N",
+    enum: {
+      values: ordenary_enums,
+      message: "{VALUE} is not among permitted values",
+    },
+  },
+  mb_views: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  mb_likes: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  mb_follow_cnt: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  mb_subscriber_cnt: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  timestamps: true, //CreatedAt, UpdatedAt
 });
 
 module.exports = mongoose.model("Member", memberSchema);
