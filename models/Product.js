cf70ad87d0dbea2f057ebce083f60ee106c39813
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-10-22 06:40:17 +0900

    feat: Develop Product schema to work with database

---
+++ models/Product.js
@@ -0,0 +1,7 @@
+const productSchema = require("../schema/product.model");
+
+class Product {
+  constructor() {
+    this.productModel = productSchema;
+  }
+}
---
+++ schema/product.model.js
@@ -0,0 +1,97 @@
+const { Schema, model } = require("mongoose");
+const {
+  product_status_enums,
+  product_collection_enums,
+  product_size_enums,
+  product_volume_enums,
+} = require("../lib/config");
+
+const productSchema = new Schema(
+  {
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_collection: {
+      type: String,
+      requiredtrue,
+      enum: {
+        values: product_collection_enums,
+        message: "{VALUE} is not among permitted enum values",
+      },
+    },
+    product_status: {
+      type: String,
+      required: true,
+      default: "PAUSED",
+      enum: {
+        values: product_status_enums,
+        message: "{VALUE} is not among permitted enum values",
+      },
+    },
+    product_price: {
+      type: Number,
+      required: true,
+    },
+    product_discount: {
+      type: Number,
+      required: true,
+    },
+    product_left_cnt: {
+      type: Number,
+      required: true,
+    },
+    product_size: {
+      type: String,
+      default: "normal",
+      required: function () {
+        const sized_list = ["dish", "salad", "dessert"];
+        return sized_list.includes(this.product_collection);
+      },
+      enum: {
+        values: product_size_enums,
+        c,
+      },
+    },
+    product_volume: {
+      type: String,
+      default: 1,
+      required: function () {
+        return this.product_collection === "drink";
+      },
+      enum: {
+        values: product_volume_enums,
+        message: "{VALUE} is not among permitted enum values",
+      },
+    },
+    product_description: {
+      type: String,
+      required: true,
+    },
+    product_image: {
+      type: Array,
+      required: false,
+      default: [],
+    },
+    product_likes: {
+      type: Number,
+      default: 0,
+    },
+    product_views: {
+      type: Number,
+      default: 0,
+    },
+    restaurant_mb_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Member",
+    },
+  },
+  { timestamps: true }
+); //createAt updateAt
+
+productSchema.index(
+  { restaurant_mb_id: 1, product_name: 1, product_size: 1, product_volume: 1 },
+  { unique: true }
+);
+
+module.exports = model("Product", productSchema);
